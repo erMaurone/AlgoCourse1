589a571167234223ae50797429f0fd88d69b4fa1
Maurone <mauro.vilasi@gmail.com>
2016-09-20 23:22:59 +0100

    refactored to comply to course conventions

--- src/main/java/lesson2/assignment/randomizedQueuesDeques/Subset.java
+++ src/main/java/lesson2/assignment/randomizedQueuesDeques/Subset.java
@@ -1,17 +1,11 @@
 package lesson2.assignment.randomizedQueuesDeques;
 
-/**
- * Created with IntelliJ IDEA.
- * User: mauro
- * Date: 09/11/2014
- * Time: 16:02
- * To change this template use File | Settings | File Templates.
- */
+
 public class Subset {
 
 
     public static void main(String[] args) {
-        RandomizedGenericQueue<String> randomStrings = new RandomizedGenericQueue<String>();
+        RandomizedQueue<String> randomStrings = new RandomizedQueue<>();
 
         if (args.length < 1) throw new IllegalArgumentException("Subject <no. of outputs> [input1, input2, ...]");
         int outputs = Integer.valueOf(args[0]);
